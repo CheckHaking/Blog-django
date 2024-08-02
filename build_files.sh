e9dcf84c610e5a1182c4789ec9fbd6ec9a1ff1df
sergiioDev <sergiomantunez@gmail.com>
2024-08-02 17:59:11 -0600

    Agregamos otra version de python a el constructor de vercel

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
+# Instala las dependencias
 pip install -r requirements.txt
 
-# make migrations
-pip install -r requirements.txt
+# Ejecuta las migraciones y recolecta los archivos estáticos
 python manage.py collectstatic --noinput
 python manage.py migrate
