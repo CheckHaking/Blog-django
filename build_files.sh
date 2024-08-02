be732f3cfae30b818b5d604443c54ba1446b3bce
sergiioDev <sergiomantunez@gmail.com>
2024-08-02 17:54:37 -0600

    Borre todas las carpetas qeu se crearon al installar allauth en el directorio

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 pip install -r requirements.txt
 
 # make migrations
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
+python manage.py migrate
