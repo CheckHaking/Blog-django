085284df9254b721fcdefc1804ad1c9cd46a58eb
sergiioDev <sergiomantunez@gmail.com>
2024-08-02 18:05:27 -0600

    Posible error en build_files

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 # Instala las dependencias
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # Ejecuta las migraciones y recolecta los archivos estáticos
 python manage.py collectstatic --noinput
