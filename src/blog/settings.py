760b1d854902651e24d7b8d2252f02a86122e948
sergiioDev <sergiomantunez@gmail.com>
2024-08-08 09:51:14 -0600

    Add .gitattributes file and normalize line endings

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -23,7 +23,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False')=='True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+#DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
--- src/build_files.sh
+++ src/build_files.sh
@@ -1,6 +1,7 @@
 echo " BUILD START"
 python3.12 -m install -r requirements.txt
 
+
 # make migrations
 python3.12 manage.py migrate 
 python3.12 manage.py collectstatic
